c054ce225d6f32b1c85077e5a27820e16a736520
David Joos <david.joos@gmail.com>
2014-04-03 15:24:19 +0100

    XSLTProcessor + install/upgrade-fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,11 @@
 
 include_recipe "php"
 
+#install an XSLTProcessor (otherwise "The XSL writer was unable to find your XSLTProcessor"-phpDocumentor.EMERGENCY will be raised)
+package "php5-xsl" do
+	action :upgrade
+end
+
 #PHP Extension and Application Repository PEAR channel
 pearhub_chan = php_pear_channel "pear.php.net" do
   action :update
@@ -29,5 +34,5 @@ php_pear "phpDocumentor" do
 	if node[:phpdoc][:version] != "latest"
 		version "#{node[:phpdoc][:version]}"
 	end
-	action :upgrade if node[:phpdoc][:version] == "latest"
+	action if node[:phpdoc][:version] == "latest" ? :upgrade : :install
 end
